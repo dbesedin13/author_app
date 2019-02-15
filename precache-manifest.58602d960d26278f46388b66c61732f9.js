0c467e7cec0f4c9817bb45ba6a88ede5a3e5106b
feroxes <feroxes@gmail.com>
2019-02-15 14:43:54 +0200

    Updates

---
+++ precache-manifest.58602d960d26278f46388b66c61732f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1c61a3a9a8d35ce534c4",
+    "url": "/authors_app/static/js/runtime~main.1c61a3a9.js"
+  },
+  {
+    "revision": "7eeaad0a409cb37dab2d",
+    "url": "/authors_app/static/js/main.7eeaad0a.chunk.js"
+  },
+  {
+    "revision": "05cd8fb802aa2f41a6fa",
+    "url": "/authors_app/static/js/2.05cd8fb8.chunk.js"
+  },
+  {
+    "revision": "7eeaad0a409cb37dab2d",
+    "url": "/authors_app/static/css/main.348519d2.chunk.css"
+  },
+  {
+    "revision": "21e31802efc1755dea9559cc2b2c88ea",
+    "url": "/authors_app/index.html"
+  }
+];
